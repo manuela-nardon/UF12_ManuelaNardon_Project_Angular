661b1efc5046799fd1670a00cf0d988d23e2a4df
manuela-nardon <manuela.nardon@mat.tn.it>
2025-05-23 11:19:10 +0200

    reated service and imported the http client

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,11 +6,13 @@ import {providePrimeNG} from 'primeng/config';
 import Aura from '@primeng/themes/aura'
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     provideAnimationsAsync(),
     providePrimeNG({
       theme:{
