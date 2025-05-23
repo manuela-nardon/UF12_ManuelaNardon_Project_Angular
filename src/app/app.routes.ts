1dd5d4f963bdcfb6958f82db529385a62a24338a
manuela-nardon <manuela.nardon@mat.tn.it>
2025-05-23 11:13:25 +0200

    added first path route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CatPostsComponent } from '../core/components/cat-posts/cat-posts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'',
+    component:CatPostsComponent,
+    pathMatch:'full'
+  }
+];
