9fe4c8474935bd012f0f6bbb57cf79c3746d4e6d
manuela-nardon <manuela.nardon@mat.tn.it>
2025-05-23 11:24:13 +0200

    added the firsts variable for the service

--- src/core/services/cat-manager/cat-manager.service.ts
+++ src/core/services/cat-manager/cat-manager.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CatManagerService {
+  #URL_random_image : string = "https://cataas.com/cat?json=true";
+  #URL_random_gif : string = "https://cataas.com/cat/gif?json=true";
+
+  #router = inject(Router);
+
+  #catList = signal<any[]>([]);
+  readonly catListComp = computed(() => this.#catList());
+
 
   constructor() { }
 }
