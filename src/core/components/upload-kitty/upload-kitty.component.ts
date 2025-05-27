dc0d6b9d416818df2de30376db9a38d6f1e2938f
manuela-nardon <manuela.nardon@mat.tn.it>
2025-05-27 09:45:43 +0200

    finnaly the post action is added

--- src/core/components/upload-kitty/upload-kitty.component.ts
+++ src/core/components/upload-kitty/upload-kitty.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Component, inject, input } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { FileUpload, FileUploadEvent, UploadEvent } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from 'primeng/button';
+import { CatManagerService } from '../../services/cat-manager/cat-manager.service';
+import { FormsModule } from '@angular/forms';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { Cat } from '../../models/cat.model';
+
+interface ITag {
+  name: string;
+}
 
 @Component({
   selector: 'app-upload-kitty',
-  imports: [],
+
+  imports: [
+    FileUpload,
+    ToastModule,
+    ButtonModule,
+    MultiSelectModule,
+    FormsModule,
+  ],
+  providers: [MessageService],
   templateUrl: './upload-kitty.component.html',
-  styleUrl: './upload-kitty.component.scss'
+  styleUrl: './upload-kitty.component.scss',
 })
 export class UploadKittyComponent {
+  catService = inject(CatManagerService);
+  tags: ITag[] = [];
+  constructor(private messageService: MessageService) {}
+  selectedTags!: ITag[];
 
+  ngOnInit() {
+    this.tags = [
+      { name: 'Funny' },
+      { name: 'Meme' },
+      { name: 'Cute' },
+      { name: 'Hungry' },
+      { name: 'Fat' },
+      { name: 'Fluffy' },
+    ];
+  }
+  #router = inject(Router);
+
+  onUpload(event: FileUploadEvent) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded',
+    });
+
+    let tmp: string[] = [];
+
+    this.selectedTags.forEach((element) => {
+      tmp.push(element.name);
+    });
+
+    let fr = new FileReader();
+    fr.readAsDataURL(event.files[0]);
+    fr.onload = () => {
+      if (fr.result) {
+        let date = new Date().toJSON();
+        this.catService.addCatPost({
+          id: '' + this.catService.catListComp().length,
+          tags: tmp,
+          url: fr.result.toString(),
+          created_at: date,
+          mimetype: 'image',
+        });
+        this.#router.navigate(['']);
+      } else {
+        console.error('erroreeee!');
+      }
+    };
+  }
 }
